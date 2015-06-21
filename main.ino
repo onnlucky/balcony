594106506844a21e409e085da3c240bec64c1089
Onne Gorter <onne@onnlucky.com>
2015-06-21 23:35:43 +0200

    add global state and time management; pumps at 10:15 in the morning

--- main.ino
+++ main.ino
@@ -31,6 +31,78 @@
 #include <SoftwareSerial.h>
 
 #define NEAR 10
+#define SECONDS_PER_DAY 86400L
+
+#define STR(x) STR2(x)
+#define STR2(x) #x
+#define assert(x) do { if (!x) { \
+Serial.println("Assertion Failed: " __FILE__ ":" STR(__LINE__) ": " #x); \
+}} while (0)
+
+struct state {
+    struct time {
+        bool set;
+        unsigned long since_midnight;
+        unsigned long last_update;
+    } time;
+    struct pump {
+        unsigned long next_time;
+        unsigned long last_time;
+        int duration;
+    } pump;
+    struct temperature {
+        unsigned long last_time;
+        float celcius;
+        float humidity;
+    } temperature;
+    struct comm {
+        unsigned long last_time;
+    } comm;
+} state;
+
+// time management
+
+void setTime(unsigned long time) {
+    state.time.since_midnight = time % SECONDS_PER_DAY;
+    state.time.last_update = millis();
+    state.time.set = true;
+}
+
+void updateTime() {
+    unsigned long now = millis();
+    unsigned long delta = now - state.time.last_update;
+    if (delta >= 1000) {
+        state.time.since_midnight += delta / 1000;
+        if (state.time.since_midnight > SECONDS_PER_DAY) {
+            state.time.since_midnight -= SECONDS_PER_DAY;
+        }
+        state.time.last_update = now / 1000 * 1000;
+    }
+}
+
+unsigned long timeDelta(unsigned long t1, unsigned long t2) {
+    return t1 - t2;
+}
+
+// returns true when t1 is after reference time
+bool isAfter(unsigned long t1, unsigned long ref) {
+    unsigned long delta = timeDelta(t1, ref);
+    return delta != 0 && delta < 1209600000L; // 14 days
+}
+
+// get a future millis timestamp for a point in the day
+// will always returns something between 0 - 24 hours from now
+unsigned long millisAtTime(int hour, int minutes) {
+    unsigned long seconds = (hour * 60L + minutes) * 60L;
+    if (seconds < state.time.since_midnight) seconds += SECONDS_PER_DAY;
+
+    unsigned long millis_till_now = state.time.since_midnight * 1000L;
+    unsigned long now = millis();
+    unsigned long future = now - millis_till_now + seconds * 1000L;
+
+    assert(isAfter(future, now));
+    return future;
+}
 
 SoftwareSerial esp(ESP_TX, ESP_RX);
 
@@ -40,8 +112,11 @@ void setup() {
 
     pinMode(PUMP_PIN, OUTPUT);
     digitalWrite(PUMP_PIN, LOW);
+
     pinMode(13, OUTPUT);
     digitalWrite(13, LOW);
+
+    Serial.println("hello");
 }
 
 // will pump up water, until water level is reached
@@ -71,9 +146,41 @@ void pump() {
         digitalWrite(PUMP_PIN, HIGH);
     }
 
+    state.pump.last_time = start;
+    state.pump.duration = millis() - start;
+
     Serial.println("pump off");
     digitalWrite(13, LOW);
     digitalWrite(PUMP_PIN, LOW);
+}
+
+void service_pump() {
+    if (!state.time.set) return;
+
+    if (!isAfter(state.pump.next_time, state.pump.last_time)) {
+        state.pump.next_time = millisAtTime(10, 15);
+        assert(isAfter(state.pump.next_time, state.pump.last_time));
+    }
+
+    if (isAfter(millis(), state.pump.next_time)) {
+        pump();
+        state.pump.next_time = millisAtTime(10, 15);
+        assert(!isAfter(state.pump.next_time, state.pump.last_time));
+    }
+}
+
+void service_temperature() {
+    if (timeDelta(millis(), state.temperature.last_time) < 5000L) return;
+
+    float celcius;
+    float humidity;
+    readAm2302(TEMP_PIN, &celcius, &humidity);
+
+    if (celcius && humidity) {
+        state.temperature.celcius = celcius;
+        state.temperature.humidity = humidity;
+        state.temperature.last_time = millis();
+    }
 }
 
 void echo(int millis) {
@@ -86,6 +193,8 @@ void echo(int millis) {
 bool resetComm() {
     esp.println("AT+RST");
     echo(1600);
+    esp.println("ATE0");
+    echo(100);
     esp.println("AT+CWMODE=1");
     echo(100);
     esp.println("AT+CWJAP=\"" SSID "\",\"" PASS "\"");
@@ -112,29 +221,45 @@ bool communicate() {
 }
 
 void loop() {
-    float celcius;
-    float humidity;
-    readAm2302(TEMP_PIN, &celcius, &humidity);
-
-    Serial.print("temperature: ");
-    Serial.print(celcius);
-    Serial.print(" humidity: ");
-    Serial.print(humidity);
-    Serial.println("");
-
-    delay(5000);
-/*
-
-
-    pump();
-
-    delay(1000);
-    resetComm();
-    while (true) {
-        delay(1000);
-        communicate();
-        delay(5000);
+    // read time from serial in HHMM
+    if (Serial.available() >= 4) {
+        int hour = (Serial.read() - '0') * 10 + Serial.read() - '0';
+        int minute = (Serial.read() - '0') * 10 + Serial.read() - '0';
+        if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
+            setTime((hour * 60L + minute) * 60L);
+            state.pump.next_time = state.pump.last_time;
+        }
     }
-*/
+
+    static unsigned long last = 0;
+    if (timeDelta(millis(), last) > 5000L) {
+        last = millis();
+        int hour = state.time.since_midnight / 3600L;
+        int minute = state.time.since_midnight / 60L % 60L;
+        Serial.print(hour / 10);
+        Serial.print(hour % 10);
+        Serial.print(minute / 10);
+        Serial.print(minute % 10);
+        Serial.print(" ");
+        Serial.println(last);
+
+        Serial.print("pump: ");
+        Serial.print(state.pump.duration);
+        Serial.print(" next: ");
+        Serial.print(timeDelta(state.pump.next_time, last) / 1000L);
+        Serial.print(" last: ");
+        Serial.println(timeDelta(last, state.pump.last_time) / 1000L);
+
+        Serial.print("temperature: ");
+        Serial.print(state.temperature.celcius);
+        Serial.print(" humidity: ");
+        Serial.print(state.temperature.humidity);
+        Serial.print(" last: ");
+        Serial.println(timeDelta(last, state.temperature.last_time) / 1000L);
+    }
+
+    updateTime();
+    service_pump();
+    service_temperature();
 }
 
